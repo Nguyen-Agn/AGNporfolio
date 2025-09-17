6cbdbcc4f54e8068f109e06a155012e8322ca3ef
Nguyen-Agn <Agnthnguyen@gmail.com>
2025-09-17 12:45:36 +0700

    Fix env

--- server/index.ts
+++ server/index.ts
@@ -5,18 +5,27 @@ import mongoose from "mongoose";
 import ViteExpress from "vite-express"
 import authRoutes from "./routes/auth.route";
 import portfolioRoutes from "./routes/portfolio.route";
+import MongoStore from 'connect-mongo'
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // session setup
-app.use(session({
-  secret: process.env.SESSION_SECRET || "dev-secret",
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false } // bật true khi deploy HTTPS
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "default_secret",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URL,
+      ttl: 14 * 24 * 60 * 60, // session 14 ngày
+    }),
+    cookie: {
+      secure: process.env.NODE_ENV === "production", // true nếu https
+      maxAge: 1000 * 60 * 60 * 24, // 1 ngày
+    },
+  }));
 
 // health check
 app.get("/api/health", (req, res) => {
